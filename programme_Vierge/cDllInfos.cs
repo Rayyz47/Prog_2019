678511c8a4ba61cfde001d69c4990ec6c6ee9073
mpmorissette <39972550+mpmorissette@users.noreply.github.com>
2019-04-09 11:45:49 -0400

    Poly

---
+++ programme_Vierge/cDllInfos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programme_Vierge
+{
+    class cDllInfos : cTemplateInfos
+    {
+        public string nameChild = "";
+        public string contentChild = "";
+        public int dateEntryChild = 0;
+
+        public override string isAuthor()
+        {
+            nameChild = "Alexis";
+            return nameChild;
+        }
+
+        public override string isContent()
+        {
+            contentChild = "wahoo!";
+            return contentChild;
+        }
+
+        public override int isDate()
+        {
+            dateEntryChild = 20190410;
+            return dateEntryChild;
+        }
+    }
+}
---
+++ programme_Vierge/cInterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programme_Vierge
+{
+    class cInterface : cTemplateInfos
+    {
+        public string nameChild = "";
+        public string contentChild = "";
+        public int dateEntryChild = 0;
+
+        public override string isAuthor()
+        {
+            nameChild = "Alexandre";
+            return nameChild;
+        }
+
+        public override string isContent()
+        {
+            contentChild = "Interface";
+            return contentChild;
+        }
+
+        public override int isDate()
+        {
+            dateEntryChild = 20190409;
+            return dateEntryChild;
+        }
+    }
+}
