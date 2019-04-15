debf135fbade8f1db4c75cb759854902c12a0a26
Darrkray <49444729+Darrkray@users.noreply.github.com>
2019-04-15 11:39:53 -0400

    Add files via upload

---
+++ programme_Vierge/frmPoly2.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace programme_Vierge
+{
+    partial class frmPoly2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExect
+            // 
+            this.btnExect.Location = new System.Drawing.Point(60, 45);
+            this.btnExect.Name = "btnExect";
+            this.btnExect.Size = new System.Drawing.Size(183, 106);
+            this.btnExect.TabIndex = 0;
+            this.btnExect.Text = "Executer Avec Un For Each";
+            this.btnExect.UseVisualStyleBackColor = true;
+            this.btnExect.Click += new System.EventHandler(this.btnExect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 106);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Executer Avec Parametre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmPoly2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnExect);
+            this.Name = "frmPoly2";
+            this.Text = "frmPoly2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExect;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ programme_Vierge/frmPoly2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace programme_Vierge
+{
+    public partial class frmPoly2 : Form
+    {
+        public static string affichageTotal = "";
+        public frmPoly2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExect_Click(object sender, EventArgs e)
+        {
+            //Creation d'une liste d'animals
+            List<cAnimal> animals = new List<cAnimal>();
+
+            cCat monChat = new cCat();
+            //LE chat a un nom qui es envoiyer dans la classe animal dans name 
+            monChat.Name = "Roger";
+            //Ajouter dans la liste de forme
+            animals.Add(monChat);
+
+            cSnake monSerpent = new cSnake();
+            //LE Serpant a un nom qui es envoiyer dans la classe animal dans name 
+            monSerpent.Name = "Bob le serpent";
+            //Ajouter dans la liste de forme
+            animals.Add(monSerpent);
+
+            cDog monChien = new cDog();
+            //LE chien a un nom qui es envoiyer dans la classe animal dans name 
+            monChien.Name = "Rex";
+            //Console.WriteLine(monChien.CapitalizedName);
+
+
+            cGermanShepard monChienPolicier = new cGermanShepard();
+            //LE GermanSherperd a un nom qui es envoiyer dans la classe animal dans name 
+            monChienPolicier.Name = "Constable Bruno";
+            //Ajouter dans la liste de forme
+            animals.Add(monChienPolicier);
+            //Ajouter dans la liste de forme
+            animals.Add(monChien);
+
+            //For each pour resortire le resultat de tout les animaux dans la liste avec l action que nous desirons decouvrire 
+            // dans ce cas ci c est juste eat et talk.
+            foreach (var animal in animals)
+            {
+                animal.Eat();
+                animal.Talk();
+            }
+            MessageBox.Show(affichageTotal);
+           
+
+
+
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cCat monChat = new cCat();
+            cSnake monSerpent = new cSnake();
+            cDog monChien = new cDog();
+            monChat.Name = "Roger";
+            monSerpent.Name = "Bob le serpent";
+            monChien.Name = "Rex";
+
+            //Ici on appelle la void MakeAnimalEat pis on lui envoiye l animal en parametre pour resortire le resultat
+            MakeAnimalEat(monChien);
+            MessageBox.Show(affichageTotal);
+            affichageTotal = "";
+            MakeAnimalEat(monChat);
+            MessageBox.Show(affichageTotal);
+            affichageTotal = "";
+            MakeAnimalEat(monSerpent);
+            MessageBox.Show(affichageTotal);
+            affichageTotal = "";
+
+        }
+
+        public void MakeAnimalEat(cAnimal animal)
+        {
+            //D'ici on ressort que ce qu on veux de animal dans ce cas ci c est eat 
+            animal.Eat();
+        }
+    }
+   
+}
