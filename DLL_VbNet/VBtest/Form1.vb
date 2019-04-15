df1654284ab47fbdbcd3fc74c0ceb6fff55adc79
Darrkray <49444729+Darrkray@users.noreply.github.com>
2019-04-15 11:38:11 -0400

    Add files via upload

---
+++ DLL_VbNet/VBtest/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports Dll_cSharpe
+Public Class Form1
+    'Utilise la dll en csharpe pour encoder le texte
+    Private Sub btnCrypt_Click(sender As Object, e As EventArgs) Handles btnCrypt.Click
+        'Instancier la DLL scharpe
+        Dim dll As New DLL()
+        'retourne le texter crypter
+        txtResult.Text = dll.cryptSimple(txtText.Text)
+        'Nettoye le texte entrer
+        txtText.Text = ""
+    End Sub
+
+    Private Sub btnUnCrypt_Click(sender As Object, e As EventArgs) Handles btnUnCrypt.Click
+        'Instancier la DLL scharpe
+        Dim dll As New DLL()
+        'retourne le texter decrypter
+        txtResult.Text = dll.unCryptSimple(txtText.Text)
+        'Nettoye le texte entrer
+        txtText.Text = ""
+    End Sub
+End Class
